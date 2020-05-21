ae3f88bfe957efdffc9e821ccc69ec232a461547
Romanchikus <13ternopil@gmail.com>
2020-05-21 23:04:11 +0300

    create_handouts

--- opencourse/static/js/course_detail.js
+++ opencourse/static/js/course_detail.js
@@ -12,23 +12,3 @@ function contact_request() {
     $.post(url)
     console.log(url)
 };
-
-function ask_enrollment() {
-    $('.ask_enrollment').on("click", function(e) {
-        e.preventDefault()
-        course = $(this).attr('data-slug')
-        console.log(course, 'course_slug')
-            // txt = document.getElementById('example').value
-        data = {
-            course: course
-        }
-        $.ajax({
-            type: "POST",
-            url: '{% url "enrollments:create" %}',
-            data: data,
-            // success: function(data) {
-            //     $('#cart_count').html(data.cart_total)
-            // }
-        })
-    })
-}
